ff2e7e3566445761d6730499f73e4afbbd3f6675
Seongil lee <modolee.si@gmail.com>
2021-02-08 00:51:01 +0900

    파일 이름 오타 수정 simpe -> simple

--- src/scheduled-tasks/scheduled-tasks.module.ts
+++ src/scheduled-tasks/scheduled-tasks.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TimelinesTask } from '@scheduled-tasks/tasks/timelines.task';
 import { BroadcastsTask } from '@scheduled-tasks/tasks/broadcasts.task';
-import { SimpleTimelinesTask } from '@scheduled-tasks/tasks/simpe-timelines.task';
+import { SimpleTimelinesTask } from '@scheduled-tasks/tasks/simple-timelines.task';
 import { TimelinesModule } from '@timelines/timelines.module';
 import { BroadcastRepository } from '@broadcasts/repositories/broadcast.repository';
 import { ScheduleModule } from '@nestjs/schedule';
