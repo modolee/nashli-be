2aa97abdb755324869aa4c3e15afd94ed2f96c30
Seongil lee <modolee.si@gmail.com>
2021-02-08 11:13:33 +0900

    Ad Free CORS 추가

--- src/common/constants/environment.constant.ts
+++ src/common/constants/environment.constant.ts
@@ -1,14 +1,17 @@
 /**
  * 환경변수에서 불러옴
  */
-const { APP_ENV, FE_HOST, FE_PORT, PORT }: NodeJS.ProcessEnv = process.env;
+const { APP_ENV, FE_HOST, FE_PORT, FE_AD_FREE_HOST, PORT }: NodeJS.ProcessEnv = process.env;
 
 let FE_URL: string;
+let FE_AD_FREE_URL: string;
 
 if (APP_ENV === 'local') {
   FE_URL = `${FE_HOST}:${FE_PORT}`;
+  FE_AD_FREE_URL = `${FE_AD_FREE_HOST}:${FE_PORT}`;
 } else {
   FE_URL = FE_HOST;
+  FE_AD_FREE_URL = FE_AD_FREE_HOST;
 }
 
-export { FE_URL, PORT };
+export { FE_URL, FE_AD_FREE_URL, PORT };
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { FE_URL, PORT } from '@common/constants/environment.constant';
+import { FE_URL, FE_AD_FREE_URL, PORT } from '@common/constants/environment.constant';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [FE_URL],
+    origin: [FE_URL, FE_AD_FREE_URL],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true,
   });
